75bdf16010923a5b0284486d6705a4cd60558cd2
S3mlin <lsemic97@gmail.com>
2024-02-20 23:56:24 +0100

    ch5 catchup

--- lists/tests.py
+++ lists/tests.py
@@ -1,19 +1,38 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from lists.models import Item
 
 from lists.views import home_page
 
 
 class HomePageTest(TestCase):
 
-    
     def test_uses_home_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
 
-    
+
+    def test_displays_all_list_item(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+        response = self.client.get("/")
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
+
+
     def test_can_save_a_POST_request(self):
+        self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+    
+    def test_only_saves_items_when_necessary(self):
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(), 0)
+
+
+    def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertRedirects(response, '/')
