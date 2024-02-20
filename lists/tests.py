73cc67a7c89a9d15bf1f3c548e7d5171ed426088
S3mlin <lsemic97@gmail.com>
2024-02-20 16:54:32 +0100

    Model for list Items and sociated migration

--- lists/tests.py
+++ lists/tests.py
@@ -8,9 +8,9 @@ from lists.views import home_page
 class HomePageTest(TestCase):
 
     
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+    def test_uses_home_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
 
     
     def test_can_save_a_POST_request(self):
