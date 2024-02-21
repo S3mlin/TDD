e3f25ffa51a952e3bd00d0680ea840b592acc103
S3mlin <lsemic97@gmail.com>
2024-02-21 21:07:52 +0100

    ch7 incrementation

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
-from lists.models import Item
+from lists.models import Item, List
 
 from lists.views import home_page
 
@@ -21,8 +21,9 @@ class ListViewTest(TestCase):
 
 
     def test_displays_all_list_item(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
+        mylist = List.objects.create()
+        Item.objects.create(text="itemey 1", list=mylist)
+        Item.objects.create(text="itemey 2", list=mylist)
         response = self.client.get("/lists/the-only-list-in-the-world/")
         self.assertContains(response, "itemey 1")
         self.assertContains(response, "itemey 2")
@@ -38,3 +39,32 @@ class NewListTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post("/lists/new", data={"item_text": "A new list item"})
         self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
+
+
+class ListAndItemModelsTest(TestCase):
+     def test_saving_and_retrieving_items(self):
+        mylist = List()
+        mylist.save()
+
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.list = mylist
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Item the second"
+        second_item.list = mylist
+        second_item.save()
+
+        saved_list = List.objects.get()
+        self.assertEqual(saved_list, mylist)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(first_saved_item.list, mylist)
+        self.assertEqual(second_saved_item.text, "Item the second")
+        self.assertEqual(second_saved_item.list, mylist)
