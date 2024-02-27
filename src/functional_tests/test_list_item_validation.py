519a8eb6e55b8825b1e80ba0cbbde244b8f600a1
S3mlin <lsemic97@gmail.com>
2024-02-27 19:09:55 +0100

    empty todo item input test

--- src/functional_tests/test_list_item_validation.py
+++ src/functional_tests/test_list_item_validation.py
@@ -7,5 +7,30 @@ from selenium.webdriver.common.keys import Keys
 class ItemValidationTest(FunctionalTest):
 
     def test_cannot_add_empty_list_items(self):
+        self.browser.get(self.live_server_url)
+        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
 
-        self.fail("write_me")
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element(By.CSS_SELECTOR, '.invalid-feedback').text,
+                "You can't have an empty list item",
+            )
+        )
+
+        self.browser.find_element(By.ID, "id_new_item").send_keys("Buy milk")
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy milk")
+
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element(By.CSS_SELECTOR, ".invalid-feedback").text,
+                "You can't have an empty list item",
+            )
+        )
+
+        self.browser.find_element(By.ID, "id_new_item").send_keys("Make tea")
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy milk")
+        self.wait_for_row_in_list_table("2: Make tea")
